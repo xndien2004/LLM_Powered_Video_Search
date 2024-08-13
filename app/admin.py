809120922e2b8761dd6bab318f4b6760a7e663bd
DadRin <xuandienk4@gmail.com>
2024-08-13 17:02:58 +0700

    update

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import Video, Frame, Context
 
 # Register your models here.
-admin.site.register(Video)
-admin.site.register(Frame)
-admin.site.register(Context) 
